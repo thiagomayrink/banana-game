ac68f1b50045d9de276501bb022215061f265900
thiagomayrink <thiagoscmpaiva@gmail.com>
2021-08-30 00:11:44 -0300

    feat: responsive GUI

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-context.font = '50px Ruslan Display';
+if (canvas.width < 450) context.font = '25px Ruslan Display';
+else context.font = '50px Ruslan Display';
 
 const dropInterval = 2000;
 
@@ -18,7 +19,7 @@ let timeToNextDrop = 0;
 let lastTime = 0;
 let animationInterval: number;
 let score = 0;
-let lifes = 4;
+let lives = 4;
 let gameOver = false;
 let drops: FallingObject[] = [];
 
@@ -73,26 +74,32 @@ function animate(timestamp: number) {
     obj.move();
     if (obj.isOutOfScreen(canvas)) {
       const { isBomb } = obj.info();
-      if (!isBomb) lifes -= 1;
+      if (!isBomb) {
+        lives--;
+        updateLives();
+      }
       deleteFallingObject(obj);
     }
 
     if (player.checkCollision(obj)) {
       const { points, isBanana, isBomb } = obj.info();
-      if (isBomb) gameOver = true;
+      if (isBomb) {
+        gameOver = true;
+        lives = 0;
+        updateLives();
+      }
       if (isBanana) score *= 2;
       if (points !== 0) score += points;
       deleteFallingObject(obj);
     }
   });
 
-  if (lifes < 1) gameOver = true;
+  if (lives < 1) gameOver = true;
 
   floor.draw();
   player.draw();
   drops.forEach(obj => obj.draw());
   drawScore();
-  drawLifes();
 
   if (!gameOver) animationInterval = window.requestAnimationFrame(animate);
   else drawGameOve();
@@ -100,24 +107,12 @@ function animate(timestamp: number) {
 
 function drawGameOve() {
   context.textAlign = 'center';
-  context.fillStyle = '#FF0000';
-  context.fillText(
-    'GAME OVER, your score is: ' + score,
-    canvas.width * 0.5,
-    canvas.height * 0.5,
-    canvas.width,
-  );
-  context.fillStyle = '#FFFFFF';
-  context.fillText(
-    'GAME OVER, your score is: ' + score,
-    canvas.width * 0.5 + 3,
-    canvas.height * 0.5 + 3,
-  );
-
-  context.fillStyle = '#FF0000';
-  context.fillText('Press SPACE to restart!', canvas.width * 0.5, canvas.height * 0.5 + 60);
-  context.fillStyle = '#FFFFFF';
-  context.fillText('Press SPACE to restart!', canvas.width * 0.5 + 3, canvas.height * 0.5 + 63);
+  context.fillStyle = '#FEFAFF';
+  context.fillText('GAME OVER', canvas.width * 0.5, canvas.height * 0.5);
+  context.fillStyle = '#3200A6';
+  context.fillText('your Score: ' + score.toString(), canvas.width * 0.5, canvas.height * 0.5 + 50);
+  context.fillStyle = '#FEFAFF';
+  context.fillText('Press SPACE!', canvas.width * 0.5, canvas.height * 0.5 + 100);
 }
 
 function drawScore() {
@@ -130,14 +125,16 @@ function drawScore() {
   context.fillText('Score: ' + score, initialX + 3, initialY + 4, canvas.width * 0.5);
 }
 
-function drawLifes() {
-  const initialX = canvas.width - canvas.width * 0.95;
-  const initialY = canvas.height - canvas.height * 0.95;
-  context.textAlign = 'left';
-  context.fillStyle = 'yellow';
-  context.fillText('Lifes: ' + lifes, initialX, initialY, canvas.width * 0.5);
-  context.fillStyle = '#FF0000';
-  context.fillText('Lifes: ' + lifes, initialX + 3, initialY + 4, canvas.width * 0.5);
+function updateLives() {
+  const lifeBar = document.querySelector('.lifeBar') as HTMLElement;
+  lifeBar.innerHTML = '';
+
+  for (let i = 1; i <= 4; i++) {
+    const life = new Image(45, 45);
+    if (i > lives) life.src = './assets/sprites/heart-empty.png';
+    else life.src = './assets/sprites/heart.png';
+    lifeBar.appendChild(life);
+  }
 }
 
 function deleteFallingObject(object: FallingObject) {
@@ -149,6 +146,7 @@ function stopAnimation(interval: number): void {
 }
 
 function startGame() {
+  updateLives();
   animate(0);
 }
 
@@ -157,7 +155,7 @@ function restartGame() {
   lastTime = 0;
   animationInterval = 0;
   score = 0;
-  lifes = 3;
+  lives = 4;
   gameOver = false;
   drops = [];
   const restart = new Promise(() => window.location.reload());
